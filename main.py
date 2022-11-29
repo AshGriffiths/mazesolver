7966c9efda288465bc4eebabc42187a8381f28ce
Ash Griffiths <46426454+AshGriffiths@users.noreply.github.com>
2022-11-29 17:04:25 +0000

    Step 5 - Done

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from tkinter import Tk, BOTH, Canvas
+from typing import Optional
 
 
 class Point(object):
     def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+        self.x: int = x
+        self.y: int = y
 
 
 class Line(object):
     def __init__(self, p1: Point, p2: Point):
-        self.__p1 = p1
-        self.__p2 = p2
+        self.__p1: Point = p1
+        self.__p2: Point = p2
 
     def draw(self, canvas: Canvas, fill_colour: str) -> None:
         canvas.create_line(
@@ -31,7 +32,7 @@ class Window(object):
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
-        self.__running = False
+        self.__running: bool = False
 
     def is_running(self) -> bool:
         return self.__running
@@ -58,18 +59,18 @@ class Window(object):
 
 class Cell(object):
     def __init__(self, win: Window):
-        self.has_left_wall = True
-        self.has_right_wall = True
-        self.has_top_wall = True
-        self.has_bottom_wall = True
-        self.visited = False
-        self._x1 = None
-        self._x2 = None
-        self._y1 = None
-        self._y2 = None
-        self._win = win
+        self.has_left_wall: bool = True
+        self.has_right_wall: bool = True
+        self.has_top_wall: bool = True
+        self.has_bottom_wall: bool = True
+        self.visited: bool = False
+        self._x1: Optional[int] = None
+        self._x2: Optional[int] = None
+        self._y1: Optional[int] = None
+        self._y2: Optional[int] = None
+        self._win: Window = win
 
-    def draw(self, x1, y1, x2, y2):
+    def draw(self, x1: int, y1: int, x2: int, y2: int):
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -87,24 +88,74 @@ class Cell(object):
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
 
+    def draw_move(self, to_cell, undo: bool = False):
+        if self._win is None:
+            return
+        if self._x1 and self._y1 and self._x2 and self._y2:
+            x_mid = (self._x1 + self._x2) // 2
+            y_mid = (self._y1 + self._y2) // 2
+
+            to_x_mid = (to_cell._x1 + to_cell._x2) // 2
+            to_y_mid = (to_cell._y1 + to_cell._y2) // 2
+
+            fill_colour = "red"
+            if undo:
+                fill_colour = "gray"
+
+            # moving left
+            if self._x1 > to_cell._x1:
+                line = Line(Point(self._x1, y_mid), Point(x_mid, y_mid))
+                self._win.draw_line(line, fill_colour)
+                line = Line(Point(to_x_mid, to_y_mid), Point(to_cell._x2, to_y_mid))
+                self._win.draw_line(line, fill_colour)
+
+            # moving right
+            elif self._x1 < to_cell._x1:
+                line = Line(Point(x_mid, y_mid), Point(self._x2, y_mid))
+                self._win.draw_line(line, fill_colour)
+                line = Line(Point(to_cell._x1, to_y_mid), Point(to_x_mid, to_y_mid))
+                self._win.draw_line(line, fill_colour)
+
+            # moving up
+            elif self._y1 > to_cell._y1:
+                line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
+                self._win.draw_line(line, fill_colour)
+                line = Line(Point(to_x_mid, to_cell._y2), Point(to_x_mid, to_y_mid))
+                self._win.draw_line(line, fill_colour)
+
+            # moving down
+            elif self._y1 < to_cell._y1:
+                line = Line(Point(x_mid, y_mid), Point(x_mid, self._y2))
+                self._win.draw_line(line, fill_colour)
+                line = Line(Point(to_x_mid, to_y_mid), Point(to_x_mid, to_cell._y1))
+                self._win.draw_line(line, fill_colour)
+
 
 def main() -> None:
     win = Window(800, 600)
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
 
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
+    c1.draw_move(c2)
 
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
     win.wait_for_close()
 
 
