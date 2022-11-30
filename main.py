830a12511f5aac7295e0eab89ea1fdc3ddc5ca77
Ash Griffiths <46426454+AshGriffiths@users.noreply.github.com>
2022-11-30 07:54:37 +0000

    Step 6 - Done

--- main.py
+++ main.py
@@ -94,38 +94,28 @@ class Cell(object):
         if self._win is None:
             return
         if self._x1 and self._y1 and self._x2 and self._y2:
-            x_mid = (self._x1 + self._x2) // 2
-            y_mid = (self._y1 + self._y2) // 2
-
-            to_x_mid = (to_cell._x1 + to_cell._x2) // 2
-            to_y_mid = (to_cell._y1 + to_cell._y2) // 2
-
             fill_colour = "red"
             if undo:
                 fill_colour = "gray"
-
-            # moving left
+            x_mid = (self._x1 + self._x2) // 2
+            y_mid = (self._y1 + self._y2) // 2
+            to_x_mid = (to_cell._x1 + to_cell._x2) // 2
+            to_y_mid = (to_cell._y1 + to_cell._y2) // 2
             if self._x1 > to_cell._x1:
                 line = Line(Point(self._x1, y_mid), Point(x_mid, y_mid))
                 self._win.draw_line(line, fill_colour)
                 line = Line(Point(to_x_mid, to_y_mid), Point(to_cell._x2, to_y_mid))
                 self._win.draw_line(line, fill_colour)
-
-            # moving right
             elif self._x1 < to_cell._x1:
                 line = Line(Point(x_mid, y_mid), Point(self._x2, y_mid))
                 self._win.draw_line(line, fill_colour)
                 line = Line(Point(to_cell._x1, to_y_mid), Point(to_x_mid, to_y_mid))
                 self._win.draw_line(line, fill_colour)
-
-            # moving up
             elif self._y1 > to_cell._y1:
                 line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
                 self._win.draw_line(line, fill_colour)
                 line = Line(Point(to_x_mid, to_cell._y2), Point(to_x_mid, to_y_mid))
                 self._win.draw_line(line, fill_colour)
-
-            # moving down
             elif self._y1 < to_cell._y1:
                 line = Line(Point(x_mid, y_mid), Point(x_mid, self._y2))
                 self._win.draw_line(line, fill_colour)
