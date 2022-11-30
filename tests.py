bb05dc1d38db49fe8913e8bdd90f9a55cdf7940e
Ash Griffiths <46426454+AshGriffiths@users.noreply.github.com>
2022-11-30 18:38:09 +0000

    Type hinted

--- tests.py
+++ tests.py
@@ -4,9 +4,9 @@ from maze import Maze
 
 
 class Tests(unittest.TestCase):
-    def test_maze_create_cells(self):
-        num_cols = 12
-        num_rows = 10
+    def test_maze_create_cells(self) -> None:
+        num_cols: int = 12
+        num_rows: int = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._cells),
